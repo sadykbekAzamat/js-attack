e5e0c3ad7a04d6c010b15a53d9e9f3dd168e033c
Azamat Sadykbek <31741@iitu.edu.kz>
2026-01-22 22:08:54 +0500

    v1

--- load-test.js
+++ load-test.js
@@ -4,7 +4,7 @@ const http = require("http");
 const config = {
   baseUrl: "https://api.aliya.kz-beta.pp.ua",
   endpoint: "/api/health",
-  requestsPerSecond: 20,
+  requestsPerSecond: 50,
   duration: 60,
   method: "GET",
   headers: {
